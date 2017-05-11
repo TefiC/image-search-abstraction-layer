8de2d34e874cce3639aebaf28b9416846c3a626b
Estefania Cass <TefiC@users.noreply.github.com>
2017-05-11 14:50:28 +0000

    Connected to mLab

--- server.js
+++ server.js
@@ -3,12 +3,28 @@ var mongo = require("mongodb");
 
 var dataURL = process.env.MONGOLAB_URI;
 
-var app = express()
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+	
+	mongo.connect(dataURL, function(err, db) {
+		if (err) throw err;
+	   
+		var recentSearchCollection = db.collection('recentSearch');
+	   
+		recentSearchCollection.insert({
+			"a":"Test"
+		}, function(err, data) {
+			if (err) throw err;
+			
+			res.end(JSON.stringify(data));
+		});
+	  
+	});  
+
+	res.send('Hello World!');
 })
 
 app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port!')
+  console.log('Example app listening on port!');
 })
